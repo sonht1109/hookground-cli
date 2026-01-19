6075217ab05c2784b91d1651e50c5aa508f35092
sonht1109 <yourgitemail@example.com>
2026-01-19 17:46:35 +0700

    update: only support macos now since im the one who using it

--- bundle.sh
+++ bundle.sh
@@ -1,11 +1,11 @@
 # Linux
-GOOS=linux GOARCH=amd64 go build -o bundles/tunnel-linux-amd64 main.go
-GOOS=linux GOARCH=arm64 go build -o bundles/tunnel-linux-arm64 main.go
+# GOOS=linux GOARCH=amd64 go build -o bundles/tunnel-linux-amd64 main.go
+# GOOS=linux GOARCH=arm64 go build -o bundles/tunnel-linux-arm64 main.go
 
 # macOS
 GOOS=darwin GOARCH=amd64 go build -o bundles/tunnel-darwin-amd64 main.go
 GOOS=darwin GOARCH=arm64 go build -o bundles/tunnel-darwin-arm64 main.go
 
 # Windows
-GOOS=windows GOARCH=amd64 go build -o bundles/tunnel-windows-amd64.exe main.go
-GOOS=windows GOARCH=arm64 go build -o bundles/tunnel-windows-arm64.exe main.go
+# GOOS=windows GOARCH=amd64 go build -o bundles/tunnel-windows-amd64.exe main.go
+# GOOS=windows GOARCH=arm64 go build -o bundles/tunnel-windows-arm64.exe main.go
